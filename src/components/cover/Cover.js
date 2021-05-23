b1980aae76fef05e4404370635e0384f02e84fff
noe <palmas.inter@gmail.com>
2021-05-22 22:45:42 -0700

    video y navbar

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -6,6 +6,8 @@ export const Cover = () => {
     return (
         <div className='cover-container'>
             <video className='video' src={coverVideo} autoPlay loop muted />
+            <h1>Noe Gonzalez Martinez</h1>
+            <p>Hi there, I am a frontend developer</p>
         </div>
     )
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+export const Navbar = ( {isScrolling} ) => {
+
+    const toTheTop = () => {
+        window.scrollTo({ top: 0, left:0, behavior: 'smooth' })
+    }
+
+    return (
+        <nav className={`navbar ${isScrolling > 20 ? 'scrolling': null}`}>
+            <div className="navbar-logo" onClick={toTheTop}>
+                Inicio
+            </div>
+        </nav>
+    )
+}
+
+
